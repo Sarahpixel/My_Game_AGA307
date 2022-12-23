0a6f2bb6c16067281d3002a8766f5f0ac6e4bb53
Sarahpixel <107088382+Sarahpixel@users.noreply.github.com>
2022-12-24 00:15:40 +1100

    win panel
    i currently hav an insta win and i currently have no knowlage on how to connect this to when there's no enemeis in scene

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -2,26 +2,64 @@ using StarterAssets;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using TMPro;
 
 
 public class UIManager : MonoBehaviour
 {
-    public GameObject gameOverPanel;
-    public GameObject player;
+    //public int score;
+    public GameObject winPanel;
 
-    //public void OnEnable()
+    //public GameObject pausePanel;
+    //public Enemy enemy;
+
+    //public TMP_Text KillScore_Text;
+
+
+
+
+    void Start()
+    {
+        //turn off our win panel object
+        winPanel.SetActive(false);
+        Cursor.lockState = CursorLockMode.Locked;
+
+
+    }
+
+    //public void EnemiesKilled()
     //{
-    //    EnemyDamage.OnPlayerDeath += EnableGameOverScreen;
-    //}
-    //public void OnDisable()
-    //{
-    //    EnemyDamage.OnPlayerDeath -= EnableGameOverScreen;
+    //    score++;
+    //    //dispalys our kill count
+    //    KillScore_Text.text = "ENEMY KILLED :" + score;
+
+    //    if (score> 0)
+    //    {
+    //        winPanel.SetActive(true);
+    //        //disable pause panel so the player can't pause the game when they've won the game
+    //        pausePanel.SetActive(false);
+    //    }
+
+
     //}
 
-    //public void EnableGameOverScreen()
-    //{
-    //    gameOverPanel.SetActive(true);
-    //    player.gameObject.GetComponent<ThirdPersonController>().enabled = false;
-    //}
+    public void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.U))
+        {
+            winPanel.SetActive(true);
+            Cursor.lockState = CursorLockMode.None;
+        }
+    }
+
+    public void ToTitleScene()
+    {
+        //hooked on the button to the title screen
+        SceneManager.LoadScene("TitleScreen");
+    }
+
+
 
 }
